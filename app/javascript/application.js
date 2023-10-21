f0b43d7a38cd2fc84cd27bb1c87e9c4e2fe935cc
秦晨 <qsc1956826@Gmail.com>
2023-10-21 21:27:41 +0800

    调整导航栏样式

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+let prevScrollTop = window.scrollY;
+window.addEventListener('scroll', (event) => {
+    const nav = document.getElementById("header-nav");
+    if (prevScrollTop > window.scrollY) {
+        nav.classList.add("active")
+    } else if (prevScrollTop < window.scrollY) {
+        nav.classList.remove("active")
+    }
+    prevScrollTop = window.scrollY
+})
