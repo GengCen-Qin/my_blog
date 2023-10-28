c76ff1d5dfaf9b31cb8b66db07148ca5405d8069
秦晨 <qsc1956826@Gmail.com>
2023-10-28 21:21:12 +0800

    增加模型：Category分类，与Post文章，呈1对多关系

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   has_many :tag_posts
   has_many :tags, through: :tag_posts
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_125206) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_131721) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "tag_posts", force: :cascade do |t|
