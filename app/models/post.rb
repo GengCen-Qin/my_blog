6ff162b0bc9b2f0891c20587dca4c65f0d1f1dd3
秦晨 <qsc1956826@Gmail.com>
2023-11-04 19:09:14 +0800

    通过回调，确保文章删除时，对应的标签或分类能及时回收

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :tag_posts
   has_many :tags, through: :tag_posts
   belongs_to :category
+
+  before_destroy :check_tag_and_category
+
+  def check_tag_and_category
+    p self.category
+    self.category.delete if (self.category.posts.count == 1)
+    self.tags.each { |tag| tag.delete if (tag.posts.count == 1) }
+  end
 end
