c0369c371331e1dd406ee582f2e25dcf87b0e878
秦晨 <qsc1956826@Gmail.com>
2023-11-04 18:42:50 +0800

    首页介绍动态化展示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,8 +4,9 @@ import "./controllers"
 
 import LocalTime from "local-time"
 LocalTime.start()
-
 let prevScrollTop = window.scrollY;
+
+// 控制导航栏展示
 window.addEventListener('scroll', (event) => {
     const nav = document.getElementById("header-nav");
     if (prevScrollTop > window.scrollY) {
@@ -15,3 +16,18 @@ window.addEventListener('scroll', (event) => {
     }
     prevScrollTop = window.scrollY
 })
+
+const textEl = document.getElementById("introduce");
+const text = textEl.innerText
+let idx = 1
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+    idx++
+    if(idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, 300)
+}
