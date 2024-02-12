1dfc26a98cb496a5e3b8bd49739d5405328e5e1d
秦思成 <464815411@qq.com>
2024-02-12 16:24:16 +0800

    使用request.js解决csrf请求头问题

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -1,29 +1,25 @@
 import {Controller} from "@hotwired/stimulus"
+import {post} from '@rails/request.js'
 
 // Connects to data-controller="file-upload"
 export default class extends Controller {
     connect() {
     }
 
-    upload(event) {
+    async upload(event) {
         event.preventDefault();
         let file = event.dataTransfer.files[0];
         let reader = new FileReader();
         reader.onload = async () => {
             let formData = new FormData();
             formData.append('file', file);
-            fetch('/fileUpload', {
-                method: 'POST',
-                body: formData,
-                credentials: 'include'
-            })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.code === 200) {
-                        let postContent = document.getElementById("post_content");
-                        postContent.value += `![${file.name}](${data.address})`
-                    }
-                });
+            const response = await post('/fileUpload', {body: formData})
+            if (response.ok) {
+                response.json.then((data) => {
+                    let postContent = document.getElementById("post_content");
+                    postContent.value += `![${file.name}](${data.address})`
+                })
+            }
         }
         reader.readAsDataURL(file);
     }
